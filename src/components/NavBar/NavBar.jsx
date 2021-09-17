33ca8dd4c4773b583cc810d01bc1300ac0bdb316
ComputerScientistAndrew <44506286+ComputerScientistAndrew@users.noreply.github.com>
2021-09-16 22:20:17 -0500

    Improved Nav bar to collapse

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 
 const NavBarComponent = () => {
   return (
-    <Nav className="nav-links justify-content-end" activeKey="/home">
-      <Nav.Item>
-        <Nav.Link>
-          <Link to="about" smooth duration={1000}>
-            About
-          </Link>
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link>
-          <Link to="projects" smooth duration={1000}>
-            Projects
-          </Link>
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link>
-          <Link to="contact" smooth duration={1000}>
-            Contact
-          </Link>
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="nav-links">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="about" smooth duration={1000}>
+                About |
+              </Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="projects" smooth duration={1000}>
+                Projects |
+              </Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="contact" smooth duration={1000}>
+                Contact
+              </Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
